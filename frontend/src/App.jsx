869ce0702bb656d57dc5336b94b080ef1da5a475
Alvaro <alvarobermejo2004@gmail.com>
2024-11-26 12:50:00 +0100

    Cambiar de nombre a la PaginaProductos a PaginaRetro y agregar nuevas páginas: PaginaFirmadas y PaginaColaboraciones. Además, añado enlaces de navegación entre toda la pagina.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,11 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Registro from './components/Registro/Registro';
 import PaginaPrincipal from './components/PaginaPrincipal/PaginaPrincipal';
 import Login from './components/Login/Login';
-import Productos from './components/PaginaProductos/PaginaProductos';
+import Productos from './components/PaginaRetro/PaginaRetro';
 import RegistrarProductos from './components/Forms/RegistrarProducto/RegistrarProductos';
 import RegistrarResenia from './components/Forms/RegistrarReseña/RegistrarResenia';
+import PaginaFirmadas from './components/PaginaFirmadas/PaginaFirmadas';
+import PaginaColaboraciones from './components/PaginaColaboraciones/PaginaColaboraciones';
 
 function App() {
   return (
@@ -16,7 +18,9 @@ function App() {
             <Route path="/" element={<Login />} />
             <Route path="/registro" element={<Registro />} />
             <Route path="/paginaPrincipal" element={<PaginaPrincipal />} />
-            <Route path="/paginaProductos" element={<Productos />} />
+            <Route path="/paginaRetro" element={<Productos />} />
+            <Route path="/paginaFirmadas" element={<PaginaFirmadas />} />
+            <Route path="/paginaColaboraciones" element={<PaginaColaboraciones />} />
             <Route path="/registrarProductos" element={<RegistrarProductos />} />
             <Route path="/registrarResenia" element={<RegistrarResenia/>} />
           </Routes>
