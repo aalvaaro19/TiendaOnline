64f9512f0473acaf193e6cf21cec2c49f89baaa1
Alvaro <alvarobermejo2004@gmail.com>
2025-02-24 12:23:07 +0100

    Mejoras en el diseño y funcionalidad de componentes: ajustes en estilos, implementación de un menú desplegable y optimización de la responsividad.

--- frontend/src/components/Nav/Nav.jsx
+++ frontend/src/components/Nav/Nav.jsx
@@ -1,22 +1,35 @@
 import '../Nav/Nav.css'
 import { Link } from 'react-router-dom';
+import Menu from '../DropdownMenu/Menu';
+
 function Nav(){
 	return (
 		<nav className='navBar'>
 			<ul className='navBar-list'>
-				<li><button className='navBar-element'>
-					<Link to="/"><p className='palabra-boton-login'>INICIO</p></Link>
-				</button></li>
-				<li><button className='navBar-element'>
-					<Link to="/paginaRetro"><p className='palabra-boton-login'>RETRO</p></Link>
-				</button></li>
-				<li><button className='navBar-element'>
-					<Link to="/paginaFirmadas"><p className='palabra-boton-login'>FIRMADAS</p></Link>
-				</button></li>
-        <li><button className='navBar-element'>
-					<Link to="/paginaColaboraciones"><p className='palabra-boton-login'>COLABORACIONES</p></Link>
-				</button></li>
+				<li className='navBar-element-li'>
+					<button className='navBar-element'>
+					<Link to="/" className='palabra-menu-drop'><p className='palabra-boton-login'>INICIO</p></Link>
+					</button>
+				</li>
+				<li className='navBar-element-li'>
+					<button className='navBar-element'>
+						<Link to="/paginaRetro" className='palabra-menu-drop'><p className='palabra-boton-login'>RETRO</p></Link>
+					</button>
+				</li>
+				<li className='navBar-element-li'>
+					<button className='navBar-element'>
+						<Link to="/paginaFirmadas" className='palabra-menu-drop'><p className='palabra-boton-login'>FIRMADAS</p></Link>
+					</button>
+				</li>
+        <li className='navBar-element-li'>
+					<button className='navBar-element'>
+						<Link to="/paginaColaboraciones" className='palabra-menu-drop'><p className='palabra-boton-login'>COLABORACIONES</p></Link>
+					</button>
+				</li>
 			</ul>
+			<div className='menu-responsive'>
+				<Menu />
+			</div>
 		</nav>
 	);
 };
