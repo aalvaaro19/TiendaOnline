7721c5bd9eaf062acd48358c03f906726b7ad9a5
NC <nicolascasasrodriguez@gmail.com>
2025-03-05 20:04:41 +0100

    responsive de la pagina principal

--- frontend/src/components/PaginaPrincipal/PaginaPrincipal.jsx
+++ frontend/src/components/PaginaPrincipal/PaginaPrincipal.jsx
@@ -5,20 +5,21 @@ import FotosApartados from '../fotosApartado/FotosApartados';
 import ContactaConNosotros from '../ContactaConNosotros/ContactaConNosotros';
 import Descripcion from '../descripcion/descripcion';
 import EscaleraFotos from '../escaleraFotos/CarruselFotos';
-import Certificacion from '../certificacion/Certificacion';
+import Certificacion from '../Certificacion/Certificacion';
+import './PaginaPrincipal.css';
 
 function PaginaPrincipal() {
   const token = localStorage.getItem('token');
   console.log(token);
   return (
-    <div>
-        <Header />
-        <FotosApartados />
-        <EscaleraFotos />
-        <Descripcion />
-        <Certificacion />
-        <ContactaConNosotros />
-        <Footer />
+    <div className="pagina-principal">
+      <Header />
+      <FotosApartados />
+      <EscaleraFotos />
+      <Descripcion />
+      <Certificacion />
+      <ContactaConNosotros />
+      <Footer />
     </div>
   )
 }
